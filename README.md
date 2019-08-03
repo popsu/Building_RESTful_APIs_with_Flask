7656ae9c674c0dd75ee31780bbeb822c9a36a892
Tommi Hovi <tommi.hovi@gmail.com>
2019-08-03 16:41:33 +0300

    Part 1 done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Building RESTful APIs with Flask 
 ### course from Linkedin Learning
+
+Uses: Python 3.7
---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/super_simple')
+def super_simle():
+    return 'Hello from the Planetary API.'
+
+
+if __name__ == '__main__':
+    app.run()
