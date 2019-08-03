57083e6f45decb92e269f6773dc95b86751d553c
Tommi Hovi <tommi.hovi@gmail.com>
2019-08-03 16:36:12 +0300

    Make containers localhost point to host

--- postman/run.sh
+++ postman/run.sh
@@ -4,6 +4,9 @@
 # Fix by running:
 # xhost +"local:docker@"
 
+# https://stackoverflow.com/questions/31324981/how-to-access-host-port-from-docker-container
+# Use --net="host" to have container's localhost point to host
+
 
 docker run  \
     --rm \
@@ -11,4 +14,5 @@ docker run  \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
     --device /dev/snd:/dev/snd \
+    --net="host" \
     postman:19.04
