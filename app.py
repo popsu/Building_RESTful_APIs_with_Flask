6c0f3874c4548e278ffc7af6854f54e741b2018e
Tommi Hovi <tommi.hovi@gmail.com>
2019-08-03 17:15:10 +0300

    Part 2 done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,8 +9,33 @@ def hello_world():
 
 
 @app.route('/super_simple')
-def super_simle():
-    return 'Hello from the Planetary API.'
+def super_simple():
+    return jsonify(message='Hello from the Planetary API.')
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That resource was not found'), 404
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = request.args.get('age', type=int)
+
+    if age < 18:
+        return jsonify(message=f'Sorry {name} you are not old enough.'), 401
+    else:
+        return jsonify(message=f'Welcome {name} you are old enough!')
+
+
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+
+    if age < 18:
+        return jsonify(message=f'Sorry {name} you are not old enough.'), 401
+    else:
+        return jsonify(message=f'Welcome {name} you are old enough!')
 
 
 if __name__ == '__main__':
