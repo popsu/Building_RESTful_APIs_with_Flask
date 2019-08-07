f5f5923a5056d261a68ecd582bd6af44f0792fa3
Tommi Hovi <tommi.hovi@gmail.com>
2019-08-07 19:26:48 +0300

    Fix script
    This option doesn't persist, so uncommenting it

--- postman/run.sh
+++ postman/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# https://stackoverflow.com/questions/43015536/xhost-command-for-docker-gui-apps-eclipse
-# Fix by running:
-# xhost +"local:docker@"
-
 # https://stackoverflow.com/questions/31324981/how-to-access-host-port-from-docker-container
 # Use --net="host" to have container's localhost point to host
 
+
+# https://stackoverflow.com/questions/43015536/xhost-command-for-docker-gui-apps-eclipse
+# Fix by running:
+xhost +"local:docker@"
 
 docker run  \
     --rm \
